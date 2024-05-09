f6fa25a31c20f45b9cf0681c4ba4fd6cfb456405
Lucas Garcia <lucas.garcia@student.unsw.edu.au>
2024-05-09 23:54:24 +1000

    portfolio finished

--- scripts/project.js
+++ scripts/project.js
@@ -10,3 +10,19 @@ function back() {
         window.location.href = "../pages/map.html";
     }, 1300);
 }
+
+function hide() {
+    var projmiddle = document.getElementsByClassName("projmiddle"); 
+    for(var i = 0; i < projmiddle.length; i++){
+        projmiddle[i].style.display = "none";
+    }
+    document.getElementById('down-button').style.display="none"
+}
+
+function show(iden) {
+    hide();
+    if (iden == 11) {
+        document.getElementById("down-button").style.display="flex";
+    }
+    document.getElementById(`info${iden}`).style.display="flex";
+}
